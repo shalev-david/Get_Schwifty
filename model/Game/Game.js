a62df190ecf102e81409dd8313c5aad082d11512
xShAlooLx <shalev.david03@gmail.com>
2022-07-03 20:17:31 +0300

    Fixed bugs on Game.js

--- model/Game/Game.js
+++ model/Game/Game.js
@@ -8,36 +8,36 @@ class Game {
     CanMove(id){
         if(id == 0) return false;
         let index = this.#GetIndexById(id);
-       if(this.#CanMoveTo([index[0]+1, index[1]])) return [index[0]+1, index[1]];
-       if(this.#CanMoveTo([index[0]-1, index[1]])) return [index[0]-1, index[1]];
-       if(this.#CanMoveTo([index[0], index[1]])) return [index[0], index[1]+1];
-       if(this.#CanMoveTo([index[0], index[1]])) return [index[0], index[1]-1];
+       if(this.#CanMoveTo(index[0]+1, index[1])) return [index[0]+1, index[1]];
+       if(this.#CanMoveTo(index[0]-1, index[1])) return [index[0]-1, index[1]];
+       if(this.#CanMoveTo(index[0], index[1]+1)) return [index[0], index[1]+1];
+       if(this.#CanMoveTo(index[0], index[1]-1)) return [index[0], index[1]-1];
 
        return false;
     }
 
     Move(id){
-        let indexOfEmptySlot = CanMove(id);
+        let indexOfEmptySlot = this.CanMove(id);
 
         if(indexOfEmptySlot){
-            board[indexOfEmptySlot[0], indexOfEmptySlot[1]] = id;
             let indexOfId = this.#GetIndexById(id);
-            board[indexOfId[0],indexOfId[1]] = 0;
+            this.board[indexOfEmptySlot[0]][indexOfEmptySlot[1]] = id;
+            this.board[indexOfId[0]][indexOfId[1]] = 0;
             return true;
         }
         else return false;
     }
 
     #CanMoveTo(row, column){
-        if(row == -1 || row == difficulty || column == -1 || column == difficulty) return false;
-        else return !board[row, column];
+        if(row == -1 || row == this.difficulty || column == -1 || column ==this. difficulty) return false;
+        else return !this.board[row][column];
     }
 
     #GetIndexById(id){
         let index;
-        for(let i=0; i< difficulty; i++){
-            if(board[i].has(id)){
-                index = [i, board[i].indexOf(id)];
+        for(let i=0; i< this.difficulty; i++){
+            if(this.board[i].includes(id)){
+                index = [i, this.board[i].indexOf(id)];
                 break;
             }          
         }
