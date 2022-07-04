3419f3236d4f441c7b5a9e536d464b6795c3bbdc
xShAlooLx <shalev.david03@gmail.com>
2022-07-04 16:21:01 +0300

    Added an option to multipile scoreboards by difficulty

--- model/ScoreBoard/ScoreBoard.js
+++ model/ScoreBoard/ScoreBoard.js
@@ -1,28 +1,28 @@
 class ScoreBoard{
-    constructor(players){
-        this.players = players
+    constructor(scoreBoard){
+        this.scoreBoard = scoreBoard
     }
 
     AddPlayerToScoreBoard(player){
-        this.players.push(player);
-        this.players.sort((firstPlayer,secondPlayer)=>{
+        if(!this.scoreBoard.has(player.difficulty)){
+            this.scoreBoard.set(player.difficulty, []);
+        }
+        let currentScoreBoard = this.scoreBoard.get(player.difficulty);
+        currentScoreBoard.push(player);
+        currentScoreBoard.sort((firstPlayer,secondPlayer)=>{
             this.#PlayersCompare(firstPlayer, secondPlayer);
         });
 
-        if(this.players.length >= 6){
-            this.players = this.players.slice(0,5);
+        if(currentScoreBoard.length >= 6){
+            currentScoreBoard = currentScoreBoard.slice(0,5);
         }
-
-        return this.players;
+        this.scoreBoard.set(player.difficulty, currentScoreBoard);
+        return currentScoreBoard;
     }
 
     #PlayersCompare(firstPlayer,secondPlayer){
-        if(firstPlayer.difficulty > secondPlayer.difficulty) return -1;
-            else if (firstPlayer.difficulty > secondPlayer.difficulty) return 1;
-            else{
-                if(firstPlayer.moves < secondPlayer.moves) return -1;
-                else if(firstPlayer.moves > secondPlayer.moves) return 1;
-            }
-            return 0;
+        if(firstPlayer.moves < secondPlayer.moves) return -1;
+        else if(firstPlayer.moves > secondPlayer.moves) return 1;
+        return 0;
     }
 }
